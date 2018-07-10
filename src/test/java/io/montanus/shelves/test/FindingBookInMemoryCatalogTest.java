8afd46a6af21e2f48941a2e62bb90cf1c161b11e
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-07-10 12:39:10 +0200

    We now handle (by an accident of implementation) not finding a book in an InMemoryCatalog.

--- src/test/java/io/montanus/shelves/test/FindingBookInMemoryCatalogTest.java
+++ src/test/java/io/montanus/shelves/test/FindingBookInMemoryCatalogTest.java
@@ -2,7 +2,9 @@ package io.montanus.shelves.test;
 
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,6 +16,18 @@ public class FindingBookInMemoryCatalogTest {
             put("::isbn for existing book::", book);
         }});
         assertEquals(book, catalog.findBook("::isbn for existing book::"));
+    }
+
+    @Test
+    public void bookNotFound() {
+        final InMemoryCatalog catalog = catalogWithout("::isbn for non existing book::");
+        assertEquals(null, catalog.findBook("::isbn for non existing book::"));
+    }
+
+    private InMemoryCatalog catalogWithout(final String isbnToAvoid) {
+        return new InMemoryCatalog(new HashMap<String, Book>() {{
+            put("Anything but " + isbnToAvoid, new Book());
+        }});
     }
 
     private static class InMemoryCatalog {
