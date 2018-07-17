8b9fd3822ae0ee7bf95cfee7a06e9f3ba8aa3ead
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-07-17 17:52:42 +0200

    We can now process several lines as long as they are all 'sane'

--- src/test/java/io/montanus/shelves/test/ui/ProcessTextPresenterTest.java
+++ src/test/java/io/montanus/shelves/test/ui/ProcessTextPresenterTest.java
@@ -2,11 +2,13 @@ package io.montanus.shelves.test.ui;
 
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.Reader;
-import java.io.StringReader;
+import java.util.stream.Stream;
 
 public class ProcessTextPresenterTest {
     @Rule
@@ -16,25 +18,80 @@ public class ProcessTextPresenterTest {
     public void zeroLines() {
         final TextCommandInterpreter interpreter = context.mock(TextCommandInterpreter.class);
         final TextSanitizer sanitizer = context.mock(TextSanitizer.class);
+        final Stream<String> lines = Stream.empty();
+
         context.checking(new Expectations() {{
+            allowing(sanitizer).sanitize(lines);
+            will(returnValue(Stream.empty()));
+
             never(interpreter);
         }});
+
         final TextProcessor processor = new TextProcessor(sanitizer, interpreter);
-        processor.process(new StringReader(""));
+        processor.process(lines);
     }
 
-    private interface TextCommandInterpreter {}
+    @Test
+    public void oneLine() {
+        final TextCommandInterpreter interpreter = context.mock(TextCommandInterpreter.class);
+        final TextSanitizer sanitizer = context.mock(TextSanitizer.class);
+        final Stream<String> lines = Stream.of("12345");
 
-    private interface TextSanitizer {}
+        context.checking(new Expectations() {{
+            allowing(sanitizer).sanitize(lines);
+            will(returnValue(Stream.of("12345")));
+
+            oneOf(interpreter).interpret(with("12345"));
+        }});
+
+        final TextProcessor processor = new TextProcessor(sanitizer, interpreter);
+        processor.process(lines);
+    }
+
+    @Test
+    public void severalLinesAllSane() {
+        final TextCommandInterpreter interpreter = context.mock(TextCommandInterpreter.class);
+        final TextSanitizer sanitizer = context.mock(TextSanitizer.class);
+        final Stream<String> lines = Stream.of("12345", "23456", "99999");
+
+        context.checking(new Expectations() {{
+            allowing(sanitizer).sanitize(lines);
+            will(returnValue(Stream.of("12345", "23456", "99999")));
+
+            oneOf(interpreter).interpret("12345");
+            oneOf(interpreter).interpret("23456");
+            oneOf(interpreter).interpret("99999");
+        }});
+
+        final TextProcessor processor = new TextProcessor(sanitizer, interpreter);
+        processor.process(lines);
+    }
+
+    private interface TextCommandInterpreter {
+        void interpret(String textCommand);
+    }
+
+    private interface TextSanitizer {
+        Stream<String> sanitize(Stream<String> lines);
+    }
 
     private static class TextProcessor {
 
+        private final TextSanitizer sanitizer;
+        private final TextCommandInterpreter interpreter;
+
         private TextProcessor(TextSanitizer sanitizer, TextCommandInterpreter interpreter) {
 
+            this.sanitizer = sanitizer;
+            this.interpreter = interpreter;
         }
 
         private void process(Reader reader) {
+            process(new BufferedReader(reader).lines());
+        }
 
+        private void process(Stream<String> lines) {
+            lines.forEach((line) -> interpreter.interpret(line));
         }
     }
 }
