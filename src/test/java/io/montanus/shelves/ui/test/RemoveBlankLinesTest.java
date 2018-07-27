84e7bd15620d59c7d50c89f21fbfc851debac672
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-07-27 12:23:31 +0200

    We can now remove blank lines for a stream with only a non blank line.

--- src/test/java/io/montanus/shelves/ui/test/RemoveBlankLinesTest.java
+++ src/test/java/io/montanus/shelves/ui/test/RemoveBlankLinesTest.java
@@ -15,9 +15,17 @@ public class RemoveBlankLinesTest {
         assertEquals(0, sanitizedLines.count());
     }
 
+    @Test
+    public void oneNonBlankLine() {
+        final BlankLineSanitizer sanitizer = new BlankLineSanitizer();
+        final Stream<String> lines = Stream.of("::line1::");
+        final Stream<String> sanitizedLines = sanitizer.sanitize(lines);
+        assertEquals("::line1::", sanitizedLines.findFirst().orElse(""));
+    }
+
     private static class BlankLineSanitizer {
         private Stream<String> sanitize(Stream<String> lines) {
-            return Stream.empty();
+            return lines;
         }
     }
 }
