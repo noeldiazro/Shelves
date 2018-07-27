f8bf490caf257a15711fc59e52776a990f29ddc3
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-07-27 17:03:07 +0200

    We can now interpret text commands as isbns.

---
+++ src/test/java/io/montanus/shelves/ui/test/InterpretTextCommandsTest.java
@@ -0,0 +1,40 @@
+package io.montanus.shelves.ui.test;
+
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class InterpretTextCommandsTest {
+    @Rule
+    public final JUnitRuleMockery context = new JUnitRuleMockery();
+
+    @Test
+    public void interpretAsIsbn() {
+        final IsbnListener listener = context.mock(IsbnListener.class);
+
+        context.checking(new Expectations() {{
+            oneOf(listener).onIsbn("::isbn::");
+        }});
+
+        final TextCommandInterpreter interpreter = new LibraryTextCommandInterpreter(listener);
+        interpreter.interpret("::isbn::");
+    }
+
+    private interface IsbnListener {
+        void onIsbn(String isbn);
+    }
+
+    private static class LibraryTextCommandInterpreter implements TextCommandInterpreter {
+        private final IsbnListener listener;
+
+        public LibraryTextCommandInterpreter(IsbnListener listener) {
+            this.listener = listener;
+        }
+
+        @Override
+        public void interpret(String textCommand) {
+            listener.onIsbn(textCommand);
+        }
+    }
+}
